618a29c48f7f3ed570c6fbb5337581cf6c24f502
evsmazhar <m.mazharwattoo@gmail.com>
2021-12-10 22:54:45 +0500

    repharased

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 from nltk.corpus import stopwords
 
-# Omkar Pathak
-NAME_PATTERN = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+# Muhammad Mazhar
+NAME_PATTERN = [{'POS': 'PROPN'}, {'POS': 'PROPN'},{"ORTH": "User"}, {"ORTH": "name"}, {"ORTH": ":"}, {}]
 
 # Education (Upper Case Mandatory)
 EDUCATION = [
-            'BSC', 'BSCS', 'BSIT', 'BSSE', 'BBIT', 'BSDS','MSDS','MSC', 'MSCS',
+            'BSC','BS Computer Science', 'BSCS', 'BSIT', 'BS Information technologies', 'BSSE','BS software engineerning', 'BBIT', 'BSDS','MSDS','MSC', 'MSCS',
             'MCS', 'MS', 'M.S', 'BTECH', 'MTECH','PHD','MPHIL','ICS' ,'FSC','ICOM','BCOM','DCOM',
-            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII',''
         ]
 
 NOT_ALPHA_NUMERIC = r'[^a-zA-Z\d]'
@@ -53,3 +53,49 @@ RESUME_SECTIONS_GRAD = [
                     'summary',
                     'leadership'
                 ]
+terms = {'Quality/Six Sigma':['black belt','capability analysis','control charts','doe','dmaic','fishbone',
+                              'gage r&r', 'green belt','ishikawa','iso','kaizen','kpi','lean','metrics',
+                              'pdsa','performance improvement','process improvement','quality',
+                              'quality circles','quality tools','root cause','six sigma',
+                              'stability analysis','statistical analysis','tqm'],      
+        'Operations management':['automation','bottleneck','constraints','cycle time','efficiency','fmea',
+                                 'machinery','maintenance','manufacture','line balancing','oee','operations',
+                                 'operations research','optimization','overall equipment effectiveness',
+                                 'pfmea','process','process mapping','production','resources','safety',
+                                 'stoppage','value stream mapping','utilization'],
+        'Supply chain':['abc analysis','apics','customer','customs','delivery','distribution','eoq','epq',
+                        'fleet','forecast','inventory','logistic','materials','outsourcing','procurement',
+                        'reorder point','rout','safety stock','scheduling','shipping','stock','suppliers',
+                        'third party logistics','transport','transportation','traffic','supply chain',
+                        'vendor','warehouse','wip','work in progress'],
+        'Project management':['administration','agile','budget','cost','direction','feasibility analysis',
+                              'finance','kanban','leader','leadership','management','milestones','planning',
+                              'pmi','pmp','problem','project','risk','schedule','scrum','stakeholders'],
+        'Data analytics':['analytics','api','aws','big data','busines intelligence','clustering','code',
+                          'coding','data','database','data mining','data science','deep learning','hadoop',
+                          'hypothesis test','iot','internet','machine learning','modeling','nosql','nlp',
+                          'predictive','programming','python','r','sql','tableau','text mining',
+                          'visualuzation'],
+         'Data Science':['analytics','api','aws','big data','busines intelligence','clustering','code',
+                          'coding','data','database','data mining','data science','deep learning','hadoop',
+                          'hypothesis test','iot','internet','machine learning','modeling','nosql','nlp',
+                          'predictive','programming','python','r','sql','tableau','text mining',
+                          'visualuzation'],
+         'Machine Learning':['analytics','api','aws','big data','busines intelligence','clustering','code',
+                          'coding','data','database','data mining','data science','deep learning','hadoop',
+                          'hypothesis test','iot','internet','machine learning','modeling','nosql','nlp',
+                          'predictive','programming','python','r','sql','tableau','text mining',
+                          'visualuzation'],
+         'Software Engineernig':['analytics','api','aws','big data','busines intelligence','clustering','code',
+                          'coding','data','database','data mining','data science','deep learning','hadoop',
+                          'hypothesis test','iot','internet','machine learning','modeling','nosql','nlp',
+                          'predictive','programming','python','r','sql','tableau','text mining',
+                          'visualuzation'],
+         'System Security':['analytics','api','aws','big data','busines intelligence','clustering','code',
+                          'coding','data','database','data mining','data science','deep learning','hadoop',
+                          'hypothesis test','iot','internet','machine learning','modeling','nosql','nlp',
+                          'predictive','programming','python','r','sql','tableau','text mining',
+                          'visualuzation'],
+        'Healthcare':['adverse events','care','clinic','cphq','ergonomics','healthcare',
+                      'health care','health','hospital','human factors','medical','near misses',
+                      'patient','reporting system']}
